da8c406e19a6bad1582c87295b0e571de1fbb541
Camieux <72339948+camieuxgoofy@users.noreply.github.com>
2022-11-01 19:29:02 +0700

    Update koneksi.php

--- Week 9/koneksi.php
+++ Week 9/koneksi.php
@@ -4,4 +4,4 @@ $koneksi = mysqli_connect("HOSTNAME", "USERNAME", "PASSWORD", "DATABASE"); //Ses
 if ($koneksi->connect_error) {
     die("Koneksi gagal!" . mysqli_connect_error());
 }
-echo 'Koneksi berhasil';
+// echo 'Koneksi berhasil';
